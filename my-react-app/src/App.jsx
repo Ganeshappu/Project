670bc5da37abeb64145aaf64c9a3c86b82bd0bdf
Darshan <bucky202206@gmail.com>
2025-04-30 11:16:31 +0530

    Create

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600 p-8">
+        Hello Tailwind + Vite + React!
+      </h1>
+      <p className="px-8 text-red-700">
+        This is a demonstration of Tailwind CSS working with Vite and React.
+      </p>
+    </div>
+  )
+}
+
+export default App
